e72d06d66a20e80ffafcce0fa94c3097b1ec9041
ToxicMonster <toxicmonster@toxicmonster.x10.mx>
2024-02-01 12:12:45 +1100

    Base script with most GVLK

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Windows Activation
- Activate All Versions of Windows
+This script is designed to activate all versions of Windows using genuine Generic Volume License Keys from Microsoft
+## Usage
+The script must be run in an elevated powershell window
+### Basic Execution
+The script can be run using the `iwr` and `iex` powershell commands
+```powershell
+iwr https://raw.githubusercontent.com/ToxicMonster/activate-windows/main/activate.ps1 | iex
+```
+### Override Current Activation
+The current product key and activation status can be overwritten using the `-OVERWRITE` switch
+```powershell
+iwr https://raw.githubusercontent.com/ToxicMonster/activate-windows/main/activate.ps1 | iex -OVERWRITE
+```
+### Specify GVLK Keys
+A custom JSON file can be specified to retrieve keys from using the `-GVLK` parameter
+```powershell
+iwr https://raw.githubusercontent.com/ToxicMonster/activate-windows/main/activate.ps1 | iex -GVLK .\config\gvlk.json
+```
+#### Key Format
+The JSON File must be formatted using the Windows Product Name and key
+```json
+{
+    "Windows Product Name": "GVLK"
+}
+```
+*Note: Some Windows OS names are not the same as their product name. For example Windows 11 has the same product name as Windows 10*
+### Specify KMS Server
+A custom KMS server can be specified using the `-KMS` parameter
+```powershell
+iwr https://raw.githubusercontent.com/ToxicMonster/activate-windows/main/activate.ps1 | iex -KMS "kms8.msguides.com"
+```
---
+++ activate.ps1
@@ -0,0 +1,90 @@
+param (
+    [string]$GVLK,
+    [string]$KMS,
+    [switch]$OVERWRITE
+)
+
+if ($GVLK) {
+    $GVLK_FILE = $GVLK
+    Write-Host "GVLK File: $GVLK"
+}
+else {
+    $GVLK_FILE = Invoke-WebRequest("https://raw.githubusercontent.com/ToxicMonster/activate-windows/main/config/gvlk.json")
+    Write-Host "GVLK File: Default"
+}
+$GVLK_FILE = Get-Content -Path $GVLK_FILE | ConvertFrom-Json
+
+if ($KMS) {
+    $KMS_SERVER = $KMS
+}
+else {
+    $KMS_SERVER = "kms8.msguides.com"
+}
+Write-Host "KMS Server: $KMS_SERVER"
+
+Write-Host "Overwrite Current Key: $OVERWRITE"
+
+function Get-WindowsSKU {
+    $osName = (Get-ComputerInfo).WindowsProductName
+    $sku = ($osName -split ':')[-1].Trim()
+    Write-Host "Product SKU: $sku"
+    return $sku
+}
+
+function Get-ProductKey($sku) {
+    $productKey = $GVLK_FILE.$sku
+    if ($productKey) {
+        Write-Host "Product Key: $productKey"
+    }
+    else {
+        Write-Error "No product key found for $sku"
+        exit
+    }
+    return $productKey
+}
+
+function Get-ActivationStatus {
+    $activationStatus = (Get-WmiObject -Query 'SELECT * FROM SoftwareLicensingProduct' | Where-Object { $_.PartialProductKey -ne $null }).LicenseStatus
+    if ($activationStatus -eq 1) {
+        Write-Host "Current Activation Status: Activated"
+        return $true
+    } elseif ($activationStatus -eq 2) {
+        Write-Host "Current Activation Status: Not Activated"
+        return $false
+    } elseif ($activationStatus -eq 3) {
+        Write-Host "Current Activation Status: Not Activated - Notification Period"
+        return $false
+    } elseif ($activationStatus -eq 4) {
+        Write-Host "Current Activation Status: Not Activated - Extended Grace Period"
+        return $false
+    } elseif ($activationStatus -eq 5) {
+        Write-Host "Current Activation Status: Not Genuine"
+        return $false
+    } else {
+        Write-Host "Current Activation Status: Error"
+        return $null
+    }
+}
+
+function Invoke-Activation($currentStatus, $productKey) {
+    if (!(($OVERWRITE -eq $false) -and ($currentStatus -eq $true))) {
+        Write-Warning "Activating Windows with product key $productKey on server $KMS_SERVER"
+        cscript.exe C:\Windows\System32\slmgr.vbs /ipk $productKey
+        cscript.exe C:\Windows\System32\slmgr.vbs /skms $KMS_SERVER
+        cscript.exe C:\Windows\System32\slmgr.vbs /ato
+        if (Get-ActivationStatus) {
+            Write-Host "Windows activated successfully with product key $productKey"
+        }
+        else {
+            Write-Error "An unknown error occurred activating Windows with product key $productKey"
+        }
+    }
+    else {
+        Write-Error "Windows is Already Activated!"
+    }
+}
+
+$currentStatus = Get-ActivationStatus
+$sku = Get-WindowsSKU
+$productKey = Get-ProductKey $sku
+Invoke-Activation $currentStatus $productKey
